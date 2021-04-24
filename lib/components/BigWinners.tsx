e5c17013bd4740b7543228244289fcf48905662e
Ryan McPeck <rmcpeck7@gmail.com>
2021-04-24 13:54:14 -0400

    displaying top 10 winners

--- lib/components/BigWinners.tsx
+++ lib/components/BigWinners.tsx
@@ -14,11 +14,10 @@ export const BigWinners = (props) => {
   const {detailedPrizes, results} = useDetailedPrizes(mdata, setBigWinners);
 
   return (
-    <Col xs={12} md={12}>
-      {results.some(x => x.isError) ? (<span>Error: {JSON.stringify(results.find(x => x.error))}</span>) : results.some(x => x.isLoading) ? (
-          <h4>Loading..</h4>
-        ) : <DisplayWinners mprizes={detailedPrizes} mresults={results} />}
-      <p>This is the BigWinners</p>
-    </Col>
+    <>
+    {results.some(x => x.isError) ? (<span>Error: {JSON.stringify(results.find(x => x.error))}</span>) : results.some(x => x.isLoading) ? (
+      <h4>Loading..</h4>
+    ) : <DisplayWinners mprizes={detailedPrizes} mresults={results} />}
+    </>
   )
 }
