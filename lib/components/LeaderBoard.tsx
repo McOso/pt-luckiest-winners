c41dec6826476fdfee362e20b797f5ccdaea5ab1
Ryan McPeck <rmcpeck7@gmail.com>
2021-05-19 07:56:39 -0400

    clean up

--- lib/components/LeaderBoard.tsx
+++ lib/components/LeaderBoard.tsx
@@ -6,10 +6,6 @@ import styles from '../../assets/styles/Home.module.css'
 export const LeaderBoard = () => {
   const {status, data, error} = usePrizePoolWinners();
 
-  // TODO: Build layout for leaderboard cards here
-  // this should be:
-  //    DESKTOP: 1 main card for #1 winner and then 3 columns of cards
-  //    MOBILE:  just 1 column of cards 
   return (
     <>
     {status === "loading" ? (<h4 className={styles.textWhite}>Loading..</h4>) : status === "error" ? (
