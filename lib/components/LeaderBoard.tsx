5c4825a3863fba473a0b136487cd2e44c3b38d7f
Ryan McPeck <rmcpeck7@gmail.com>
2021-08-23 13:17:21 -0400

    add spinner

--- lib/components/LeaderBoard.tsx
+++ lib/components/LeaderBoard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { usePrizePoolWinners } from '../hooks/usePrizePoolWinners';
 import { BigWinners } from './BigWinners';
 import styles from '../../assets/styles/Home.module.css'
+import { Loader } from './Loader';
 
 export const LeaderBoard = () => {
   const prizePoolWrapper = usePrizePoolWinners();
@@ -9,7 +10,7 @@ export const LeaderBoard = () => {
   return (
     <>
     {(prizePoolWrapper.prizePoolsV1.status || prizePoolWrapper.prizePoolsV3.status || prizePoolWrapper.prizePoolsPoly.status) === "loading" 
-      ? (<h4 className={styles.textWhite}>Loading..</h4>) 
+      ? (<Loader />) 
       : (prizePoolWrapper.prizePoolsV1.error || prizePoolWrapper.prizePoolsV3.error || prizePoolWrapper.prizePoolsPoly.error) === "error" 
       ? (
           <span className={styles.textWhite}>Error: Something went wrong..</span>
---
+++ lib/components/Loader.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+export const Loader = () => {
+
+  return (
+    <div className="loader">
+      <div className="loader-inner">
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
